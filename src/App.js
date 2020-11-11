fff8376d8928b939d132fe5b674c8aaeda7d352b
Amelia Marie Hampton <ameliahampton@Amelias-MacBook-Air.local>
2020-11-11 17:36:29 -0600

    Finished home page and added projects page, still need to complete animation transition on homepage.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 //import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, CssBaseline}   from '@material-ui/core';
+import { Grid, CssBaseline } from '@material-ui/core';
 import Header from './components/header';
 import Footer from './components/footer';
 import Main from './components/main';
@@ -9,25 +9,26 @@ import mkebackground from './components/tombarrettmkebackground.jpg';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      backgroundSize: 'cover',
-      backgroundImage: `url(${mkebackground})`,
-      backgroundRepeat: 'no-repeat',
+        minHeight: '100vh',
+        backgroundSize: 'cover',
+        backgroundImage: `url(${mkebackground})`,
+        backgroundRepeat: 'no-repeat',
     },
-      }));
+}));
 
-export default function App(){
-     {
-         const classes= useStyles();
+export default function App() {
+    {
+        const classes = useStyles();
         return (
             <React.Fragment>
-            <CssBaseline />
-            <Grid className={classes.root}>
-                <Header/>
-                     <Main/>
-                     <Footer/>
-            </Grid>
-        </React.Fragment>          
+                <CssBaseline />
+                <Grid className={classes.root}>
+                    <Header />
+                    <Main />
+                    <Footer />
+                </Grid>
+            </React.Fragment>
         );
     }
 }
- 
+
--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, IconButton, Collapse, Container, Grid, Card, CardActionArea, CardMedia} from '@material-ui/core';
+import { IconButton, Collapse, Container, Grid, Card, CardActionArea } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Radium, {StyleRoot} from 'radium';
+import { StyleRoot } from 'radium';
 import PropTypes from 'prop-types';
+import { Link as Scroll } from 'react-scroll';
 
 const useStyles = makeStyles({
     root: {
@@ -17,14 +18,12 @@ const useStyles = makeStyles({
         margin: 'auto',
     },
     grid: {
-    padding: '50px',
-    width: '100%',
+        padding: '50px',
+        width: '100%',
     },
     media: {
         borderRadius: '50%',
-        //backgroundImage: ("../images/temppicture.jpg"),
         height: '250px',
-        //boxShadow: '0px 10px 13px -7px #000000, -7px 38px 10px 19px rgba(255,255,255,0.74)',
     },
     bubble: {
         width: '250px',
@@ -34,15 +33,15 @@ const useStyles = makeStyles({
         margin: 'auto',
     },
     icon: {
+        color: '#fff',
         fontSize: '2rem',
-        color: 'black',
     },
     button: {
         color: 'black',
         fontFamily: 'Nunito',
     },
     colorText: {
-        color:'black',
+        color: 'black',
     },
     container: {
         textAlign: 'center',
@@ -55,72 +54,54 @@ const useStyles = makeStyles({
         color: 'black',
         fontSize: '3rem',
         padding: '10px',
-      
+
     },
     goDown: {
-        color:'black',
+        color: 'black',
         fontSize: '2rem',
     },
-    floating: {
-        animation: '$floating 3000ms ${theme.transitions.easing.easeInOut}',
-        animationName: 'floating',
-        animationDuration: '3s',
-        animationIterationCount: 'infinite',
-        animationTimingFuntion: 'ease-in-out',
-    },
-    "@keyframes floating": { 
-        "0%" : { opacity: 0, transform: "translateY(-200%)" }, 
-        "50%": { opacity: 1, transform: "translateY(0)" }, 
-        "100%":   { opacity: 2, transform: "translateY(0)" },     
-    } 
 });
 
-export default function Home(props) {
+export default function Home() {
 
-    const classes  = useStyles();
+    const classes = useStyles();
     const [checked, setChecked] = useState(false);
     useEffect(() => {
         setChecked(true);
-            
-        },[])
+    }, [])
 
     return (
-<Grid className={classes.root}>
-        <Container className={classes.grid}
-        >
-        <StyleRoot>
-            <Grid style={classes.bounce}>
-                <Card className={classes.bubble} margin={'auto'}>
-                    <CardActionArea>
-                    </CardActionArea>
-                </Card>
-  
-  
-         
-        
+        <Grid className={classes.root}>
+            <Container className={classes.grid}
+            >
+                <StyleRoot>
+                    <Grid style={classes.bounce}>
+                        <Card className={classes.bubble} margin={'auto'}>
+                            <CardActionArea>
+                            </CardActionArea>
+                        </Card>
 
-<Collapse in={checked} {...(checked ? {timeout: 1000 } : {})} collapsedHeight={50}>
-<div className={classes.container}>
-    <h1 className={classes.title}>
-        Kenneth Bontempo<br />
-        <br/>
-         <span className={classes.colorText}> Electrical Engineer</span>
-    </h1>
-    <IconButton>
-     <h3 className={classes.button}>
-        Follow Me 
+                        <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})} collapsedHeight={50}>
+                            <div className={classes.container}>
+                                <h1 className={classes.title}>
+                                    Kenneth Bontempo<br />
+                                    <br />
+                                    <span className={classes.colorText}> Electrical Engineer</span>
+                                </h1>
+                              
+                                    <IconButton>
+                                        <h3 className={classes.button}>
+                                            Follow Me </h3>
+                                        <ExpandMoreIcon className={classes.goDown} />
+                                    </IconButton>
+                            </div>
+                        </Collapse>
+                    </Grid>
+                </StyleRoot>
 
-         </h3> 
-        <ExpandMoreIcon className={classes.goDown} />
-    </IconButton>
-    </div>
-    </Collapse>
-    </Grid>  
-    </StyleRoot>
- 
-    </Container>  
-    </Grid>
-    
+            </Container>
+        </Grid>
+
     )
 };
 
